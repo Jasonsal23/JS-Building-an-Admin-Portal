9dcff2de4420d49f98df8732536d329faeed29b9
Jason <jason@Jasons-MacBook-Air.local>
2023-05-23 14:38:38 -0700

    admin.js

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,8 @@
+async function main(){
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
+    books.forEach(renderBook)
+}
+
 
 // Your Code Here
